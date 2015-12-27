ee1330cd3e4e19a45232b2c3738001440ce5982d
ahannan54 <ahannan54@gmail.com>
2015-12-27 23:29:48 +0300

    Crystal fall game is the first game by shiekh studio feel free to steal the code

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDestroyer : MonoBehaviour {
+
+    public GameObject destructionpoint;
+
+	// Use this for initialization
+	void Start () {
+
+        destructionpoint = GameObject.Find("DestructionPoint");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.y > destructionpoint.transform.position.y)
+        {
+            gameObject.SetActive(false);
+        }
+	
+	}
+}
---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour {
+
+    public GameObject pooledobject;
+    public int pooledvalue;
+    List<GameObject> pooledobjects;
+	// Use this for initialization
+	void Start () {
+
+        pooledobjects = new List<GameObject>();
+
+        for (int i = 0; i < pooledvalue; i++)
+        {
+            GameObject obj = new GameObject();
+            obj = (GameObject)Instantiate(pooledobject);
+            obj.SetActive(false);
+
+            pooledobjects.Add(obj);
+        }
+
+	}
+
+    public GameObject getgameobject()
+    {
+        for (int i = 0; i < pooledobjects.Count; i++)
+        {
+            if (!pooledobjects[i].activeInHierarchy)
+            {
+                return pooledobjects[i];
+            }
+        }
+
+        GameObject obj = new GameObject();
+        obj = (GameObject)Instantiate(pooledobject);
+        obj.SetActive(false);
+
+        return obj;
+
+    }
+}
